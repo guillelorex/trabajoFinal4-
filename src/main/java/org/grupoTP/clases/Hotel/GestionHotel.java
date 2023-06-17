0e4cfb5735b375f37686bd2776d9d8268dc4afa1
Guillermo lorenzo <guillermolorenzo89@gmail.com>
2023-06-16 22:04:14 -0300

    arranque la funcion de dibujo de hotel

--- src/main/java/org/grupoTP/clases/Hotel/GestionHotel.java
+++ src/main/java/org/grupoTP/clases/Hotel/GestionHotel.java
@@ -1,30 +1,27 @@
 package org.grupoTP.clases.Hotel;
 
 import org.grupoTP.Repositorios.RepoHotel;
+import org.grupoTP.clases.Hotel.Habitacion;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class GestionHotel {
 
-    RepoHotel gestion = new RepoHotel();
+    RepoHotel hotel = new RepoHotel();
+
+    List<Habitacion> listaHabitaciones = hotel.listar();
 
     public void cambiarEstado(Habitacion habitacion){
 
        Scanner scan = new Scanner(System.in);
        int opcion;
         do{
-
-        System.out.println("Ingrese el estado actual de la habitacion");
-        System.out.println("1- Disponible");
-        System.out.println("2- Ocupada");
-        System.out.println("3- Reservada");
-        System.out.println("4- Mantenimiento");
-        System.out.println("5- Fuera de servicio");
-        System.out.println("0- cancelar modificacion");
-        opcion = scan.nextInt();
+            opcionesMenuEstado();
+            opcion = scan.nextInt();
 
             switch (opcion) {
-                case 1 -> habitacion.setEstado(Habitacion.EstadoHabitacion.MANTENIMIENTO);
+                case 1 -> habitacion.setEstado(Habitacion.EstadoHabitacion.DISPONIBLE);
                 case 2 -> habitacion.setEstado(Habitacion.EstadoHabitacion.OCUPADA);
                 case 3 -> habitacion.setEstado(Habitacion.EstadoHabitacion.RESERVADA);
                 case 4 -> habitacion.setEstado(Habitacion.EstadoHabitacion.MANTENIMIENTO);
@@ -34,21 +31,89 @@ public class GestionHotel {
             }
         }while(opcion!=0);
         scan.close();
-        gestion.modificar(habitacion);
-
     }
 
     public void opcionesMenuEstado()
     {
-        System.out.println("1. Listar Habitaciones");
-        System.out.println("2. Agregar Habitacion");
-        System.out.println("3. Buscar Habitacion");
-        System.out.println("4. Cambiar Estado de Habitacion");
-        System.out.println("5. Modificar Habitacion");
-        System.out.println("6. Eliminar Habitacion");
-        System.out.println("0. Volver a Menu Principal");
+        System.out.println("Ingrese el estado actual de la habitacion");
+        System.out.println("1- Disponible");
+        System.out.println("2- Ocupada");
+        System.out.println("3- Reservada");
+        System.out.println("4- Mantenimiento");
+        System.out.println("5- Fuera de servicio");
+        System.out.println("0- cancelar modificacion");
         System.out.println("Seleccione una opcion: ");
     }
+
+    //region Contadores
+
+    public int contarHabitaciones(List<Habitacion> listaHabitaciones){
+        int contador = 0;
+        for (Habitacion habitacion : listaHabitaciones) {
+            contador++;
+        }
+        return contador;
+    }
+
+    public int contarHabitacionesPorPiso(List<Habitacion> listaHabitaciones, int piso){
+        int contador = 0;
+        for (Habitacion habitacion : listaHabitaciones) {
+            if(habitacion.getPiso() == piso){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public int contarPisos(List<Habitacion> listaHabitaciones){
+        int contador = 0;
+        for (Habitacion habitacion : listaHabitaciones) {
+            if(habitacion.getPiso() > contador){
+                contador = habitacion.getPiso();
+            }
+        }
+        return contador;
+    }
+
+
+    //endregion
+
+    public void hotelASCII(List<Habitacion> Habitaciones){
+
+        int izqTecho = 201;
+        int techo = 61;
+        int derTecho = 187;
+        int pared = 186;
+        int ventana = 205;
+
+        int pisos = contarPisos(Habitaciones);
+        int ultimoPiso = contarHabitacionesPorPiso(Habitaciones, contarPisos(Habitaciones));
+
+        // Dibujar techo del Hotel
+        System.out.printf("%d",izqTecho);
+        System.out.printf("%d",techo);
+        for (int i = 0; i < ultimoPiso; i++) {
+            System.out.printf("%d",techo);
+            System.out.printf("%d",techo);
+        }
+        System.out.printf("%d",derTecho);
+
+        System.out.println(); // Salto de línea
+
+        // Dibujar pared del hotel (sin los bordes)
+        for (int i = pisos; i > 0; i--) {
+            System.out.printf("%d",pared); // Borde izquierdo pared
+            // Habitaciones por piso
+            int habPorPiso = contarHabitacionesPorPiso(Habitaciones, i);
+            for (int j = 0; j < habPorPiso; j++) {
+                System.out.printf(" %d",ventana);
+            }
+            System.out.print(" ");
+            System.out.printf("%d",pared); // Borde derecho pared
+        }
+        System.out.println(); // Salto de línea
+    }
+
 
     // la creacion de el hotel en crear arreglo de habitaciones.
 
